0524932c5c7d1085e1459e3a00381eed8b4b1882
Luis Roberto Perez <luis.robertop87@gmail.com>
2015-10-29 16:32:34 -0400

    Updated version on pom and aplly Format on the code

--- gildedrose/src/test/java/GildedRoseTest.java
+++ gildedrose/src/test/java/GildedRoseTest.java
@@ -1,12 +1,12 @@
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		assertTrue(true);
-	}
+    @Test
+    public void testTheTruth() {
+        assertTrue(true);
+    }
 }
