61a062f7a9169018b8cae2977c31073d3b146d74
Luis Roberto <roberto.perez@swissbytes.ch>
2016-11-19 19:08:23 -0400

    Implemented algorithm for mix behavior, case Backstage Passes

---
+++ gildedrose/src/main/java/edu/gildedrose/MixerQuality.java
@@ -0,0 +1,18 @@
+package edu.gildedrose;
+
+/**
+ * Created by Luis Roberto Perez on 19/11/2016.
+ */
+public class MixerQuality implements Strategy {
+    public void updateQuality(Item item) {
+        if (item.getSellIn() == 0) {
+            item.setQuality(0);
+        } else {
+            int delta = delta = (item.getSellIn() <= 10 && item.getSellIn() > 5)
+                    ? 2 : (item.getSellIn() <= 5 && item.getSellIn() > 0)
+                    ? 3 : 0;
+
+            item.setQuality(item.getQuality() + delta);
+        }
+    }
+}
