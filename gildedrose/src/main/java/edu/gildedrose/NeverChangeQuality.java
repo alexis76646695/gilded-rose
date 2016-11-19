631404f3eba3e789c55d32799d2a6c835dd73352
Luis Roberto <roberto.perez@swissbytes.ch>
2016-11-19 19:00:45 -0400

    Implemented Algorithm to keep Quality

---
+++ gildedrose/src/main/java/edu/gildedrose/NeverChangeQuality.java
@@ -0,0 +1,10 @@
+package edu.gildedrose;
+
+/**
+ * Created by Luis Roberto Perez on 19/11/2016.
+ */
+public class NeverChangeQuality implements Strategy {
+    public void updateQuality(Item item) {
+        // nothing to do
+    }
+}
