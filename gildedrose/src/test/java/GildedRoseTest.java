12a8a07bb37780ca8ac9c30daa149b3962072585
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2018-06-25 20:18:16 -0400

    First test added

--- gildedrose/src/test/java/GildedRoseTest.java
+++ gildedrose/src/test/java/GildedRoseTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import org.junit.Test;
 
 
@@ -8,5 +9,15 @@ public class GildedRoseTest {
 	@Test
 	public void testTheTruth() {
 		assertTrue(true);
+	}
+
+	@Test
+	public void testConjuredItem() {
+		GildedRose.items = new ArrayList<Item>(0);
+		Item conjured = new Item("Conjured Mana Cake", 3, 6);
+		GildedRose.items.add(conjured);
+		GildedRose.updateQuality();
+
+		assertEquals(4, conjured.quality);
 	}
 }
